4f607c5a0569e5a0dada30a228720ff075b3c7b6
Surbhi <surbhikumaridav@gmail.com>
2022-08-07 17:01:06 +0530

    search component complete

--- src/route/search/search.component.jsx
+++ src/route/search/search.component.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import TextField from '@mui/material/TextField';
 import { Button, Tab } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search'
 import Tabs from '@mui/material/Tabs';
 import './search.styles.css'
 import { makeStyles } from '@material-ui/core';
+import axios from 'axios'
+import SingleContent from '../../components/singleContent/singleContent.component';
+import CustomPagination from '../../components/pagination/pagination.component';
 const useStyles = makeStyles({
   root: {
     position: "fixed",
     bottom: 0,
-    zIndex: 100,
+    // zIndex: 100,
     width:"60%",
     backgroundColor:"#d1d8e0",
   },
@@ -23,10 +26,25 @@ const Search = () => {
   const classes=useStyles()
   const [type,setType]=useState(0)
   const [page, setPage] = useState(1)
+  const [searchText,setSearchText]=useState("")
+  const [content,setContent]=useState([])
+  const [pages,setPages]=useState(0)
   const handleChange = (event, newValue) => {
     setType(newValue);
     setPage(1)
   }
+
+  const fetchApi=async()=>{
+    const {data} = await axios.get(`https://api.themoviedb.org/3/search/${type?"tv":"movie"}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`)
+    setContent(data.results)
+    setPages(data.total_pages)
+  }
+  useEffect(() => {
+    window.scroll(0,0)
+    fetchApi()
+    // eslint-disable-next-line
+  }, [type,page])
+  
   return (
     <>
     <div className="textfield">
@@ -36,13 +54,15 @@ const Search = () => {
           label="Search movies OR series"
           className={classes.root}
           style={{fontWeight:"500"}}
-          inputProps={{style: {fontSize: 14, fontWeight:500, color:"lightblue"}}}
+          inputProps={{style: {fontSize: 14, fontWeight:500, color:"black"}}}
           InputLabelProps={{style: {fontSize: 14,fontWeight:500, color:"black"}}} 
+          onChange={(e)=>setSearchText(e.target.value)}
         />
         <Button 
+          onClick={fetchApi}
           variant='contained' 
           style={{marginLeft:"8px" , 
-          backgroundColor:"#4b4b4b"}}
+          backgroundColor:"#d1ccc0"}}
           className={classes.button}
         >
             <SearchIcon style={{color:"white"}}/>
@@ -61,6 +81,28 @@ const Search = () => {
       <Tab style={{width:"50%", color:"white"}} label="search movies"/>
       <Tab style={{width:"50%" ,color:"white"}} label="search tv series"/>
     </Tabs>
+    <div className="card">
+        {
+          content && content.map((t)=>(
+            <SingleContent 
+            key={t.id} 
+            id={t.id}
+            poster={t.poster_path}
+            title={t.title || t.name } 
+            date={t.first_air_date || t.release_date}
+            media={t.media_type}
+            vote={t.vote_average}
+            />
+          ))
+        }
+        {
+          searchText && !content &&
+          (type?<h2>No Movies Found</h2>: <h2>No Series Found</h2> )
+        }
+      </div>
+      {
+        pages>1 && (<CustomPagination setPage={setPage} pages={pages} pageNo={page}/>)
+      }
     </>
   )
 }
